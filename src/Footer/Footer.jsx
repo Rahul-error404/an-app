f84620e4f7f99624922d9c8f1fbccf6ee94150b8
rahul mudhiraj <rmudiraj3@gmail.com>
2022-05-31 12:29:55 +0530

    first commit

---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+import React from "react";
+
+function template() {
+  return (
+    <div className="footer bg-secondary text-center text-white ">
+      &copy; rights belongs to noOne ;)
+    </div>
+  );
+};
+
+export default template;
---
+++ src/Menu/Contact.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Contact = () => {
+    return (
+        <div>
+            <h1>this is not  a contact....... or is it ?</h1>
+        </div>
+    )
+}
